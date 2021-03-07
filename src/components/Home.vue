7d62960583ab4c178d462c279ddf7f343807a8eb
yiyu66 <1226572422@qq.com>
2021-03-07 13:59:56 +0800

    新增退出登录

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,19 @@
 <template>
-    <div>
-        home
-    </div>
+  <div>
+    <el-button type="info" @click="logout"> 退出 </el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login') // 跳转登录界面
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
