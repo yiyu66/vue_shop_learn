10d97947475cdf34d98a6fd848e86f9e9605361d
yiyu66 <1226572422@qq.com>
2021-03-07 22:04:08 +0800

    侧边栏处理

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
 
-Vue.config.productionTip = false
-Vue.prototype.$http = axios
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/' // 配置请求的根目录
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 
+Vue.prototype.$http = axios
+Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App)
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,11 +4,27 @@ import {
   Form,
   FormItem,
   Input,
-  Message
+  Message,
+  Header,
+  Container,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
 Vue.prototype.$message = Message
