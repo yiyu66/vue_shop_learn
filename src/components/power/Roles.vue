51195ddac7d2d33adfe12f52d26f2869f9402898
yiyu66 <1226572422@qq.com>
2021-04-08 23:41:51 +0800

    权限界面美化

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -8,11 +8,34 @@
     <!-- 卡片视图 -->
     <el-card>
       <el-row>
-        <el-button type="primary">添加角色</el-button>
+        <el-button type="primary" @click="addRoleVisible = true">添加角色</el-button>
       </el-row>
       <!-- 角色列表区域 -->
       <el-table :data="roleList" border stripe>
-        <el-table-column type="expand"></el-table-column>
+        <!-- 展开列 -->
+        <el-table-column type="expand">
+          <template v-slot="scope">
+            <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '','vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag>{{ item1.authName }}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 渲染2、3级权限 -->
+              <el-col :span="19">
+                <el-row :class="['bdbottom', i2 === 0 ? 'bdtop' : '','vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag type="success">{{ item2.authName }}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag type="warning" v-for="item3 in item2.children" :key="item3.id">{{ item3.authName }}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
         <el-table-column label="#" type="index"></el-table-column>
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
         <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
@@ -25,6 +48,31 @@
         </el-table-column>
       </el-table>
     </el-card>
+    <!-- 添加角色的对话框 -->
+    <el-dialog title="添加用户" :visible.sync="addRoleVisible" width="30%" @close="addDiadlogClosed">
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addRoleVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 修改角色对话框 -->
+    <!-- <el-dialog title="修改用户" :visible.sync="editdialogVisible" @close="editDialogClosed" width="50%">
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
+        <el-form-item label="用户名">
+          <el-input v-model="editForm.username" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="editForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editdialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUserInfo">确 定</el-button>
+      </span>
+    </el-dialog> -->
   </div>
 </template>
 <script>
@@ -32,7 +80,8 @@ export default {
   data () {
     return {
       // 角色列表数据
-      roleList: []
+      roleList: [],
+      addRoleVisible: false
     }
   },
   created () {
@@ -46,9 +95,29 @@ export default {
       }
       this.roleList = res.data
       console.log(this.roleList)
+    },
+    addUser () {
+      this.addDialogVisible = false
+      this.getUserList()
+    },
+    addDiadlogClosed () {
+
     }
   }
 }
 </script>
 <style lang="less" scoped>
+.el-tag {
+  margin: 7px;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+.vcenter{
+  display: flex;
+  align-items: center;
+}
 </style>
