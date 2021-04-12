912b0f31e6bb06b3019601704a14d249e95d39ef
yiyu66 <1226572422@qq.com>
2021-04-13 00:11:04 +0800

    权限分配、删除

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -15,21 +15,38 @@
         <!-- 展开列 -->
         <el-table-column type="expand">
           <template v-slot="scope">
-            <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '','vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+            <el-row
+              :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']"
+              v-for="(item1, i1) in scope.row.children"
+              :key="item1.id"
+            >
               <!-- 渲染一级权限 -->
               <el-col :span="5">
-                <el-tag>{{ item1.authName }}</el-tag>
+                <el-tag closable @close="removeRightsById(scope.row, item1.id)">{{ item1.authName }}</el-tag>
                 <i class="el-icon-caret-right"></i>
               </el-col>
               <!-- 渲染2、3级权限 -->
               <el-col :span="19">
-                <el-row :class="['bdbottom', i2 === 0 ? 'bdtop' : '','vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                <el-row
+                  :class="['bdbottom', i2 === 0 ? 'bdtop' : '', 'vcenter']"
+                  v-for="(item2, i2) in item1.children"
+                  :key="item2.id"
+                >
                   <el-col :span="6">
-                    <el-tag type="success">{{ item2.authName }}</el-tag>
+                    <el-tag type="success" closable @close="removeRightsById(scope.row, item2.id)">{{
+                      item2.authName
+                    }}</el-tag>
                     <i class="el-icon-caret-right"></i>
                   </el-col>
                   <el-col :span="18">
-                    <el-tag type="warning" v-for="item3 in item2.children" :key="item3.id">{{ item3.authName }}</el-tag>
+                    <el-tag
+                      type="warning"
+                      v-for="item3 in item2.children"
+                      :key="item3.id"
+                      closable
+                      @close="removeRightsById(scope.row, item3.id)"
+                      >{{ item3.authName }}</el-tag
+                    >
                   </el-col>
                 </el-row>
               </el-col>
@@ -40,10 +57,12 @@
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
         <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
         <el-table-column label="操作" width="300px">
-          <template v-slot="">
+          <template v-slot="scope">
             <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
             <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
-            <el-button type="warning" size="mini" icon="el-icon-settings">分配</el-button>
+            <el-button type="warning" size="mini" icon="el-icon-settings" @click="showSetRightDialog(scope.row)"
+              >分配权限</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
@@ -55,24 +74,22 @@
         <el-button type="primary" @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
-    <!-- 修改角色对话框 -->
-    <!-- <el-dialog title="修改用户" :visible.sync="editdialogVisible" @close="editDialogClosed" width="50%">
-      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
-        <el-form-item label="用户名">
-          <el-input v-model="editForm.username" disabled></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱" prop="email">
-          <el-input v-model="editForm.email"></el-input>
-        </el-form-item>
-        <el-form-item label="手机号" prop="mobile">
-          <el-input v-model="editForm.mobile"></el-input>
-        </el-form-item>
-      </el-form>
+    <!-- 分配权限对话框 -->
+    <el-dialog title="分配权限" :visible.sync="setRightDialogVisible" width="50%" @close="setRightDiadlogClosed">
+      <!-- 树形组件 -->
+      <el-tree
+        :data="rightsList"
+        :props="treeProps"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+        :default-checked-keys="defKeys"
+      ></el-tree>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="editdialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="editUserInfo">确 定</el-button>
+        <el-button @click="setRightDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="setRightDialogVisible = false">确 定</el-button>
       </span>
-    </el-dialog> -->
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -81,7 +98,16 @@ export default {
     return {
       // 角色列表数据
       roleList: [],
-      addRoleVisible: false
+      addRoleVisible: false,
+      setRightDialogVisible: false,
+      rightsList: [],
+      // 树形控件的树形绑定对象
+      treeProps: {
+        label: 'authName',
+        children: 'children'
+      },
+      // 默认选中的节点ID值数组
+      defKeys: []
     }
   },
   created () {
@@ -102,6 +128,44 @@ export default {
     },
     addDiadlogClosed () {
 
+    },
+    // 删除用户权限
+    async removeRightsById (role, rightId) {
+      const confirmResult = await this.$confirm('此操作将永久删除该文件，是否继续？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('取消了删除')
+      }
+      const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+      if (res.meta.status !== 200) {
+        this.$message.error('删除权限失败')
+      }
+      role.children = res.data
+    },
+    // 控制 分配权限对话框显示
+    async showSetRightDialog (role) {
+      const { data: res } = await this.$http.get('rights/tree')
+      if (res.meta.status !== 200) {
+        this.$message.error('获取权限数据失败')
+      }
+      this.rightsList = res.data
+      this.getLeafKeys(role, this.defKeys)
+
+      this.setRightDialogVisible = true
+    },
+    // 递归函数获取角色下三级权限，defKeys
+    getLeafKeys (node, arr) {
+      if (!node.children) {
+        return arr.push(node.id)
+      }
+      node.children.forEach(item => this.getLeafKeys(item, arr))
+    },
+    setRightDiadlogClosed () {
+      this.defKeys = []
     }
   }
 }
@@ -116,7 +180,7 @@ export default {
 .bdbottom {
   border-bottom: 1px solid #eee;
 }
-.vcenter{
+.vcenter {
   display: flex;
   align-items: center;
 }
